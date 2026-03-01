a5b1e5b0cc3303890c2178446e7aa8e5eb2cae07
Abstractmelon <abstractmelons@gmail.com>
2026-03-01 12:56:33 -0700

    refactor: remove unused channel view components

--- src/lib/components/chat/channels/TextChannelView.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import { MessageList, MessageInput } from '$lib/components/chat';
-	import { activeChannel } from '$lib/stores/community';
-</script>
-
-<div class="flex-1 flex flex-col min-h-0">
-	<MessageList channelId={$activeChannel?.id ?? ''} />
-	<MessageInput channelId={$activeChannel?.id ?? ''} />
-</div>
